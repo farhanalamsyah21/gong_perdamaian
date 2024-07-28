47cc46689d436b0eb437383867ced9e921579839
farhanalamsyah21 <farhanrizkyalamsyah@gmail.com>
2024-07-28 19:54:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gong</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mdb.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+
+<!-- Intro settings -->
+<style>
+    /* Default height for small devices */
+    #intro-example {
+        height: 400px;
+    }
+
+    /* Height for devices larger than 992px */
+    @media (min-width: 992px) {
+        #intro-example {
+            height: 600px;
+        }
+    }
+
+    .gap-1 {
+        gap: 1rem;
+    }
+</style>
+
+<body>
+    <!--Main Navigation-->
+<header>
+<style>
+    /* Default height for small devices */
+    #intro-example {
+        height: 400px;
+    }
+
+    /* Height for devices larger than 992px */
+    @media (min-width: 992px) {
+        #intro-example {
+            height: 600px;
+        }
+    }
+
+    .gap-1 {
+        gap: 1rem;
+    }
+
+    body {
+        background-color: black; /* Ubah warna ini sesuai keinginan Anda */
+        color: white; /* Tambahkan ini agar teks tetap terlihat pada background hitam */
+    }
+
+        .navbar-custom {
+            background-color: #007FFF; /* Warna biru azure */
+        }
+        
+</style>
+
+  <style>
+    #intro {
+      background-image: url("https://mdbootstrap.com/img/new/fluid/city/018.jpg");
+      height: 100vh;
+    }
+
+    /* Height for devices larger than 576px */
+    @media (min-width: 992px) {
+      #intro {
+        margin-top: -58.59px;
+      }
+    }
+
+    .navbar .nav-link {
+      color: #DEB887!important;
+    }
+  </style>
+
+<style>
+        body {
+            background-color: black; /* Background hitam */
+            color: white; /* Teks berwarna putih */
+            font-family: Arial, sans-serif;
+        }
+        .content-wrapper {
+            padding: 20px;
+            max-width: 800px;
+            margin: auto;
+        }
+        .content-wrapper h4 {
+            text-align: center;
+            font-size: 1.5rem;
+            margin-bottom: 1rem;
+        }
+        .content-wrapper p {
+            text-align: justify;
+            font-size: 1rem;
+            line-height: 1.6;
+            margin-bottom: 1rem;
+        }
+        .content-wrapper .highlight {
+            color: #F0FFFF; /* Warna biru muda */
+        }
+    </style>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+            <!-- Container wrapper -->
+            <div class="container">
+                <!-- Navbar brand -->
+                <a class="navbar-brand me-2" href="/">
+                    <img src="src/assets/logo.png" height="50" alt="MDB Logo" loading="lazy" style="margin-top: -1px;" />
+                </a>
+
+                <!-- Toggle button -->
+                <button data-mdb-collapse-init class="navbar-toggler" type="button" data-mdb-target="#modalbox" aria-controls="modalbox" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+
+                <!-- Collapsible wrapper -->
+                <div class="collapse navbar-collapse" id="modalbox">
+                    <!-- Left links -->
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex gap-1">
+                        <li class="nav-item">
+                        <a data-mdb-ripple-init class="btn px-3 me-2" href="login.php" role="button" style="background-color: 	#F0FFFF; border-color: 	#F0FFFF; color: BLACK;">HOME</a>
+                        </li>
+
+                    </ul>
+
+                    <!-- Left links -->
+
+                    <div class="d-flex align-items-center">
+                    <a data-mdb-ripple-init class="btn px-3 me-2" href="login.php" role="button" style="background-color: 	#F0FFFF; border-color: 	#F0FFFF; color: BLACK;">SIGN UP</a>
+                        <a data-mdb-ripple-init class="btn px-3 me-2" href="login.php" role="button" style="background-color: 	#F0FFFF; border-color: 	#F0FFFF; color: BLACK;">LOGIN</a>
+
+                        <a data-mdb-ripple-init class="btn btn-dark px-3" href="https://github.com/mdbootstrap/mdb-ui-kit" role="button"><i class="fab fa-github"></i></a>
+                    </div>
+                </div>
+                <!-- Collapsible wrapper -->
+            </div>
+            <!-- Container wrapper -->
+        </nav>
+
+
+
+ <!-- Background image -->
+ <div id="intro-example" class="p-5 text-center bg-image" style="background-image: url('src/assets/gong 1.jpg');">
+            
+                <div class="d-flex justify-content-center align-items-center h-100">
+                    <div class="text-white">
+                        <h1 class="mb-3">GONG PERDAMAIAN</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+</header>
+<!--Main Navigation-->
+
+<!--Main layout-->
+<main class="mt-5">
+  <div class="container">
+    <!--Section: Content-->
+    <section>
+      <div class="row">
+        <div class="col-md-6 gx-5 mb-4">
+          <div class="bg-image hover-overlay shadow-2-strong" data-mdb-ripple-init data-mdb-ripple-color="light">
+            <img src="src/assets/gong 2.jpg" class="img-fluid" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 gx-5 mb-4">
+          <h4><strong>SEJARAH SINGKAT</strong></h4>
+          <p class="text-muted">
+          Gong Perdamaian Ambon merupakan monumen yang sarat makna, dibangun sebagai simbol rekonsiliasi dan perdamaian setelah konflik yang terjadi di Ambon pada akhir abad ke-20. Bunyi gong yang menggema menjadi pengingat akan pentingnya hidup berdampingan secara damai dan saling menghormati antar sesama..</p>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="row">
+        <div class="col-md-6 gx-5 mb-4">
+          <div class="bg-image hover-overlay shadow-2-strong" data-mdb-ripple-init data-mdb-ripple-color="light">
+            <img src="src/assets/gong 3.jpg" class="img-fluid" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 gx-5 mb-4">
+          <h4><strong>LATAR BELAKANG</strong></h4>
+          <p class="text-muted">
+          Latar Belakang Konflik
+Sebelum adanya Gong Perdamaian, Ambon pernah dilanda konflik horizontal yang berkepanjangan. Perbedaan agama dan suku menjadi pemicu terjadinya kekerasan dan pertumpahan darah. Konflik ini mengakibatkan kerugian yang sangat besar, baik secara materi maupun non-materi.
+
+Pembangunan Gong Perdamaian
+Sebagai upaya untuk menyembuhkan luka dan membangun kembali persatuan, pemerintah dan masyarakat Ambon sepakat untuk membangun Gong Perdamaian. Monumen ini diresmikan pada tanggal 25 November 2009 oleh Presiden Susilo Bambang Yudhoyono.
+
+Pemilihan gong sebagai simbol perdamaian memiliki alasan yang kuat. Dalam budaya Indonesia, khususnya di Maluku, gong memiliki nilai spiritual yang tinggi. Bunyi gong dianggap mampu menyatukan hati dan pikiran manusia, serta menciptakan suasana yang sakral dan damai.</p>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="row">
+        <div class="col-md-6 gx-5 mb-4">
+          <div class="bg-image hover-overlay shadow-2-strong" data-mdb-ripple-init data-mdb-ripple-color="light">
+            <img src="src/assets/gong 4.jpg" class="img-fluid" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 gx-5 mb-4">
+          <h4><strong>MAKNA DAN TUJUAN</strong></h4>
+          <p class="text-muted">
+          Gong Perdamaian Ambon memiliki beberapa makna dan tujuan, antara lain:
+
+Simbol Rekonsiliasi: Gong Perdamaian menjadi tanda bahwa masyarakat Ambon telah memaafkan dan melupakan masa lalu yang kelam.
+Pengingat Sejarah: Monumen ini mengingatkan generasi muda akan pentingnya menjaga perdamaian dan menghindari konflik.
+Harapan untuk Masa Depan: Gong Perdamaian menjadi simbol harapan bagi masyarakat Ambon untuk membangun masa depan yang lebih baik, di mana semua warga hidup dalam kedamaian dan keharmonisan.
+Titik Kumpul Masyarakat: Gong Perdamaian menjadi tempat bagi masyarakat untuk berkumpul, berdialog, dan saling memahami.</p>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="row">
+        <div class="col-md-6 gx-5 mb-4">
+          <div class="bg-image hover-overlay shadow-2-strong" data-mdb-ripple-init data-mdb-ripple-color="light">
+            <img src="src/assets/gong 5.jpg" class="img-fluid" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 gx-5 mb-4">
+          <h4><strong>LOKASI</strong></h4>
+          <p class="text-muted">
+          Lokasi dan Fitur
+Gong Perdamaian Ambon terletak di pusat kota dan mudah diakses oleh masyarakat. Monumen ini didesain dengan bentuk yang unik dan menarik, sehingga menjadi salah satu destinasi wisata yang populer di Ambon. Selain gong utama, di sekitar lokasi juga terdapat berbagai fasilitas pendukung, seperti taman, tempat duduk, dan area bermain anak.
+
+Pesan Moral
+Gong Perdamaian Ambon memberikan pesan moral yang sangat penting, yaitu:
+
+Pentingnya Perdamaian: Perdamaian adalah anugerah yang harus dijaga dan diperjuangkan.
+Toleransi: Kita harus saling menghormati perbedaan agama, suku, dan budaya.
+Persatuan: Kita harus bersatu padu untuk membangun masa depan yang lebih baik.
+          </p>
+        </div>
+      </div>
+    </section>
+
+   
+
+
+  
+<!--Footer-->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!--Footer-->
+
+
+    <script src="js/mdb.min.js"></script>
+</body>
+
+</html>
